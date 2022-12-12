87e9d3612a9cd497ea24b1bfc5af036656d97574
AjjRibera <andre.ribera@mykenzie.snhu.edu>
2022-12-12 08:28:17 -0700

    cache client save point

--- Application/src/main/java/com/kenzie/appserver/caching/CacheClient.java
+++ Application/src/main/java/com/kenzie/appserver/caching/CacheClient.java
@@ -1,10 +1,35 @@
 package com.kenzie.appserver.caching;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+
+import java.util.concurrent.TimeUnit;
+
 public class CacheClient {
+    private Cache<String, String> cache;
 
-    public CacheClient(){}
+    public CacheClient(int expiry, TimeUnit timeUnit){
+        this.cache = CacheBuilder.newBuilder()
+            .expireAfterWrite(expiry, timeUnit)
+            .concurrencyLevel(Runtime.getRuntime().availableProcessors())
+            .build();
+    }
 
+    public Concert get(String key) {
+        // Write your code here
+        // Retrieve and return the concert
+        return cache.getIfPresent(key);
+    }
 
+    public void evict(String key) {
+        // Write your code here
+        // Invalidate/evict the concert from cache
+        cache.invalidate(key);
+    }
 
-
+    public void add(String key, Concert value) {
+        // Write your code here
+        // Add concert to cache
+        cache.put(key, value);
+    }
 }
