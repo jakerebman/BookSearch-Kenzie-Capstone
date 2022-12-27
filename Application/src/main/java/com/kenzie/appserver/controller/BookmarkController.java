96b19ee82f283d5861308666ba8f746636a53021
Patrice Thomas <patrice.thomas@mykenzie.snhu.edu>
2022-12-26 21:25:14 -0500

    remove unnecessary import statement

--- Application/src/main/java/com/kenzie/appserver/controller/BookmarkController.java
+++ Application/src/main/java/com/kenzie/appserver/controller/BookmarkController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.awt.print.Book;
 import java.net.URI;
 import java.util.List;
 
